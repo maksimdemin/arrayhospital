53da40b822dc4938a012010c625fb3a42d0f164d
MaksimDemin <mdeminalex@icloud.com>
2019-08-27 00:08:40 +0800

    change HW hospital

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,21 +1,29 @@
+import java.text.DecimalFormat;
+
 public class Hospital {
 
     public static void main(String[] args) {
-
+        DecimalFormat formatter = new DecimalFormat("#0.0 ℃");
         //double[] hospital = new double[2];
-        double[] hospital = {36.6, 36.8, 32.0, 36.9, 40.0, 37.2, 38.1, 39.6, 39.9, 36.2, 36.6, 36.8, 39.9, 40.0, 32.6, 36.7, 39.9, 37.3, 38.4, 36.3, 33.5, 38.0, 39.4, 36.7, 40.0, 33.8, 39.3, 36.4, 36.8, 40.0};
-        System.out.println(hospital.length);
+        //double[] hospital = {36.6, 36.8, 32.0, 36.9, 40.0, 37.2, 38.1, 39.6, 39.9, 36.2, 36.6, 36.8, 39.9, 40.0, 32.6, 36.7, 39.9, 37.3, 38.4, 36.3, 33.5, 38.0, 39.4, 36.7, 40.0, 33.8, 39.3, 36.4, 36.8, 40.0};
+        double[] hospital = new double[30];
+        for (int j = 0; j < hospital.length; j++) {
+           hospital[j] = (32 + Math.random() * 8);
+            System.out.println(formatter.format(hospital[j]));
+        }
+
+        //System.out.println(hospital.length);
         double sumTemperature = 0;
         int countGoodHealth = 0;
         for (int i = 0; i < hospital.length; i++) {
             sumTemperature += hospital[i];
             if (hospital[i] >= 36.2 && hospital[i] <= 36.9) {
+                System.out.println("Temperature's health man = " + formatter.format(hospital[i]));
                 countGoodHealth++;
             }
         }
-        double middleTemperature = (Math.round(sumTemperature * 1.0) / 1.0) / hospital.length;
-        System.out.println("Hospital average body temperature = " + middleTemperature);
+        double middleTemperature = sumTemperature / hospital.length;
         System.out.println("The number of healthy people: " + countGoodHealth + " peoples");
-
+        System.out.println("Hospital average body temperature = " + formatter.format(middleTemperature));
     }
 }

034df39e3fd0651fa6db826e1d26347ee4be1b5a
MaksimDemin <mdeminalex@icloud.com>
2019-08-27 00:28:57 +0800

    change HW final 26.08.2019

--- src/Hospital.java
+++ src/Hospital.java
@@ -3,16 +3,15 @@ import java.text.DecimalFormat;
 public class Hospital {
 
     public static void main(String[] args) {
-        DecimalFormat formatter = new DecimalFormat("#0.0 ℃");
-        //double[] hospital = new double[2];
+        DecimalFormat formatter = new DecimalFormat("#0.0 °C"); // Округляем и форматируем выводимые значения для удобочитаемости. Используем для этого класс DecimalFormat для форматирования десятичных чисел в строку
+
         //double[] hospital = {36.6, 36.8, 32.0, 36.9, 40.0, 37.2, 38.1, 39.6, 39.9, 36.2, 36.6, 36.8, 39.9, 40.0, 32.6, 36.7, 39.9, 37.3, 38.4, 36.3, 33.5, 38.0, 39.4, 36.7, 40.0, 33.8, 39.3, 36.4, 36.8, 40.0};
         double[] hospital = new double[30];
         for (int j = 0; j < hospital.length; j++) {
            hospital[j] = (32 + Math.random() * 8);
-            System.out.println(formatter.format(hospital[j]));
+            System.out.println(hospital[j]);
         }
 
-        //System.out.println(hospital.length);
         double sumTemperature = 0;
         int countGoodHealth = 0;
         for (int i = 0; i < hospital.length; i++) {
